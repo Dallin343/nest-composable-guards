18f7a8a6491368325023828f5802ae310a132799
Dallin Hagman <dallinhagman@gmail.com>
2024-01-17 20:37:13 -0700

    Finished all operators except 'Not'

--- src/composable.guard.ts
+++ src/composable.guard.ts
@@ -1,36 +1,164 @@
-import { CanActivate, ExecutionContext, Inject, Injectable } from '@nestjs/common';
-import { Reflector, ModuleRef } from '@nestjs/core';
-import { from, Observable, of } from 'rxjs';
-import { COMPOSABLE_GUARD_KEY } from './constants';
+import {
+  applyDecorators,
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  SetMetadata,
+  Type,
+  UseGuards,
+} from "@nestjs/common";
+import { Reflector, ModuleRef } from "@nestjs/core";
+import {
+  map,
+  concat,
+  forkJoin,
+  from,
+  Observable,
+  of,
+  switchMap,
+  takeWhile,
+} from "rxjs";
+import { COMPOSABLE_GUARD_KEY } from "./constants";
+import { Composition, isComposition, isType } from "./operators";
+
+export function Compose(
+  composition: Composition
+): ClassDecorator & MethodDecorator {
+  return SetMetadata(COMPOSABLE_GUARD_KEY, composition);
+}
+
+export function UseComposeGuard(
+  composition: Composition
+): ClassDecorator & MethodDecorator {
+  return applyDecorators(Compose(composition), UseGuards(ComposableGuard));
+}
 
 @Injectable()
 export class ComposableGuard implements CanActivate {
-    constructor(
-        @Inject(Reflector) private readonly reflector: Reflector,
-        @Inject(ModuleRef) private readonly moduleRef: ModuleRef) {}
+  constructor(
+    @Inject(Reflector) private readonly reflector: Reflector,
+    @Inject(ModuleRef) private readonly moduleRef: ModuleRef
+  ) {}
 
-    
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+  canActivate(
+    context: ExecutionContext
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const composition = this.reflector.get<Composition>(
+      COMPOSABLE_GUARD_KEY,
+      context.getClass()
+    );
+
+    // Maps from type symbol to an instance of that type
+    let guardInstances: Record<string, Promise<CanActivate>> = {};
+
+    // let guardInstances: Promise<CanActivate>[] = [];
+    composition.types.forEach((type) => {
+      try {
+        guardInstances[type.toString()] = this.moduleRef.get(type);
+      } catch (err) {
+        guardInstances[type.toString()] = this.moduleRef.create(type);
+      }
+    });
+
+    // Wait for all guard instances to be retrieved or created
+    return forkJoin(guardInstances).pipe(
+      switchMap((guards) => this.process(composition, guards, context))
+    );
+  }
+
+  process(
+    composition: Composition,
+    guards: Record<string, CanActivate>,
+    context: ExecutionContext
+  ): Observable<boolean> {
+    switch (composition.op) {
+      case 'all':
+        return this.processAll(composition, guards, context);
+      case 'any':
+        return this.processAny(composition, guards, context);
+      case 'oneof':
+        return this.processOneOf(composition, guards, context);
+      case 'not':
+        return this.processNot(composition, guards, context);
     }
+  }
 
-    deferGuard(guard: CanActivate, context: ExecutionContext): Observable<boolean> {
-        const result = guard.canActivate(context);
-
-        if (this.isPromise(result)) {
-            return from(result);
+  processAny(
+    composition: Composition,
+    guards: Record<string, CanActivate>,
+    context: ExecutionContext
+  ): Observable<boolean> {
+    return concat(composition.terms).pipe(
+      switchMap((term) => {
+        if (isType(term)) {
+          return this.deferGuard(guards[term.toString()], context);
         }
-        else if (this.isObservable(result)) {
-            return result;
+        return this.process(term, guards, context);
+      }),
+      takeWhile((termResult) => termResult !== true)
+    );
+  }
+
+  processAll(
+    composition: Composition,
+    guards: Record<string, CanActivate>,
+    context: ExecutionContext
+  ): Observable<boolean> {
+    return forkJoin(
+      composition.terms.map((term) => {
+        if (isType(term)) {
+          return this.deferGuard(guards[term.toString()], context);
         }
-        return of(result);
-    }
+        return this.process(term, guards, context);
+      })
+    ).pipe(map((results) => results.every((result) => result)));
+  }
 
-    isPromise(val: boolean | Promise<boolean> | Observable<boolean>): val is Promise<boolean> {
-        return (val as Promise<boolean>).then !== undefined;
-    }
+  processOneOf(
+    composition: Composition,
+    guards: Record<string, CanActivate>,
+    context: ExecutionContext
+  ): Observable<boolean> {
+    return forkJoin(
+      composition.terms.map((term) => {
+        if (isType(term)) {
+          return this.deferGuard(guards[term.toString()], context);
+        }
+        return this.process(term, guards, context);
+      })
+    ).pipe(map((results) => results.filter((result) => result).length === 1));
+  }
 
-    isObservable(val: boolean | Observable<boolean>): val is Observable<boolean> {
-        return (val as Observable<boolean>).pipe !== undefined;
+  processNot(
+    composition: Composition,
+    guards: Record<string, CanActivate>,
+    context: ExecutionContext
+  ): Observable<boolean> {
+    return of(true);
+  }
+
+  deferGuard(
+    guard: CanActivate,
+    context: ExecutionContext
+  ): Observable<boolean> {
+    const result = guard.canActivate(context);
+
+    if (this.isPromise(result)) {
+      return from(result);
+    } else if (this.isObservable(result)) {
+      return result;
     }
-    
+    return of(result);
+  }
+
+  isPromise(
+    val: boolean | Promise<boolean> | Observable<boolean>
+  ): val is Promise<boolean> {
+    return (val as Promise<boolean>).then !== undefined;
+  }
+
+  isObservable(val: boolean | Observable<boolean>): val is Observable<boolean> {
+    return (val as Observable<boolean>).pipe !== undefined;
+  }
 }
--- src/operators.ts
+++ src/operators.ts
@@ -1,34 +1,52 @@
 import { CanActivate, Type } from "@nestjs/common";
 
-export type Composed = {
-    operator: 'or' | 'and' | 'xor' | 'not'
-    terms: (Composed | Type<CanActivate>)[]
+type OpType = "any" | "all" | "oneof" | "not";
+
+export type Composition = {
+  op: OpType;
+  terms: (Composition | Type<CanActivate>)[];
+  types: Set<Type<CanActivate>>;
+};
+
+export type CompositionTerm = Composition | Type<CanActivate>;
+
+export function isComposition(val: CompositionTerm): val is Composition {
+  return (val as Composition).op !== undefined;
 }
 
-export type CompositionTerm = 
-    Type<CanActivate> | ((...args: CompositionTerm[]) => Composed);
+export function isType(val: CompositionTerm): val is Type<CanActivate> {
+  return (val as Type<CanActivate>).call !== undefined;
+}
 
-export function Or(...terms: CompositionTerm[]): Composed {
-    let composed: Composed = {
-        operator: 'or',
-        terms: []
-    };
+function Operator(op: OpType, terms: CompositionTerm[]): Composition {
+  let composed: Composition = {
+    op,
+    terms,
+    types: new Set(),
+  };
 
-    for (let term of terms) {
-        
+  for (let term of terms) {
+    if (isComposition(term)) {
+      term.types.forEach((type) => composed.types.add(type));
+    } else if (isType(term)) {
+      composed.types.add(term);
     }
-
-    return composed;
+  }
+  return composed;
 }
 
-export function And(...terms: CompositionTerm[]) {
-
+export function Any(...terms: CompositionTerm[]): Composition {
+  return Operator("any", terms);
 }
 
-export function Xor(...terms: CompositionTerm[]) {
+export function All(...terms: CompositionTerm[]): Composition {
+  return Operator("all", terms);
+}
 
+export function OneOf(...terms: CompositionTerm[]): Composition {
+  return Operator("oneof", terms);
 }
 
 export function Not(...terms: CompositionTerm[]) {
-
+  return Operator("not", terms);
 }
